e3a8bf834debe2ee4a3d180131d83b93721ecd14
R Sheetal <r.sheetal_1@signify.com>
2020-01-03 16:33:52 +0530

    added gitignore file

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const respond = require('./lib/respond.js');
 
 const server = http.createServer(respond);
 
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
 server.listen(port, () => {
    console.log("Listening"); 
